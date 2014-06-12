f114eeb99f6e04dd5fd15b4c273f4dfa2b986805
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-06-12 12:28:28 -0500

    Added tests for "db"

--- test/db-spec.js
+++ test/db-spec.js
@@ -1,20 +1,33 @@
 var chai = require("chai");
 var expect = chai.expect;
 
-var storage = require("dom-storage");
-global.localStorage = new storage("./test-db.json", { strict: false, ws: "  " });
+//var storage = require("dom-storage");
+//global.localStorage = new storage("./test/test-db.json", { strict: false, ws: "  " });
 
 var db = require("../lib/db")();
 
 describe("db", function()
 {
+	var nameTableId = db.tableIds.name;
+	var nameTable = db.getTable(nameTableId);
+
 	describe("getTable(tableId)", function()
 	{
 		it("gets a table by id", function()
 		{
-			var nameTableId = db.tableIds.name;
 			var nameTable = db.getTable(nameTableId);
 			expect(nameTable).to.not.be.undefined;
+		});
+	});
+
+	describe("insert(tableId, data)", function()
+	{
+		it("inserts data into the table specified by "+
+			"tableId and returns data", function()
+		{
+			db.insert(nameTableId, { name: "Jeff" });
+			var recordCount = nameTable({ name: "Jeff" }).count();
+			expect(recordCount).to.equal(1);
 		});
 	});
 });
