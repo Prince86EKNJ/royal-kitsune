f49d5bf1bc7e22fbb99c384ce7ef593a93af8cbb
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-06-19 17:59:02 -0500

    Attemping to mock I/O for fox-repl test

--- test/fox-repl.js
+++ test/fox-repl.js
@@ -1,1 +1,25 @@
+var chai = require("chai");
+var expect = chai.expect;
+
 var foxRepl = require("../lib/fox-repl")();
+
+describe("fox-repl", function()
+{
+	describe("start()", function()
+	{
+		it("starts the fox shell", function()
+		{
+			var stream = require("stream");
+			var outStream = new stream.Stream();
+			outStream.writeable = true;
+			outStream.write = function() { /* NOP */ };
+			outStream.end = function() { /* NOP */ };
+
+			var replSession = foxRepl.start({
+				output: outStream
+			});
+
+			expect(replSession).to.not.be.undefined;
+		});
+	});
+});
