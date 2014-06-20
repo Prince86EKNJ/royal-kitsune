68202329776a37c3b19dd87ea1db10bd39172f8f
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-06-20 11:41:53 -0500

    Added more testing for "fox-repl"

--- test/fox-repl.js
+++ test/fox-repl.js
@@ -1,7 +1,10 @@
 var chai = require("chai");
 var expect = chai.expect;
 
-var foxRepl = require("../lib/fox-repl")();
+var stream = require("stream");
+var StringDecoder = require("string_decoder").StringDecoder;
+
+var foxRepl = require("../lib/fox-repl")(null, { msg: "Hello" });
 
 describe("fox-repl", function()
 {
@@ -9,15 +12,25 @@ describe("fox-repl", function()
 	{
 		it("starts the fox shell", function()
 		{
-			var stream = require("stream");
-			var outStream = new stream.Stream();
-			outStream.writeable = true;
-			outStream.write = function() { /* NOP */ };
-			outStream.end = function() { /* NOP */ };
+			var data = "";
+			var outStream = new stream.Writable();
+			outStream._write = function(chunk)
+			{
+				var decoder = new StringDecoder("utf8");
+				data += decoder.write(chunk);
+				return true;
+			};
 
 			var replSession = foxRepl.start({
-				output: outStream
+				input: process.stdin,
+				output: outStream,
+				terminal: true
 			});
+
+			replSession.eval(":name", null, null, function() {});
+			replSession.eval(".exit");
+
+			replSession.rli.close();
 
 			expect(replSession).to.not.be.undefined;
 		});
