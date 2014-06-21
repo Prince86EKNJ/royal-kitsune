7a1a97a8bad6e52e04b867aa1eebcdc870616552
Edward Nicholes Jr <Prince86EKNJ@gmail.com>
2014-06-21 12:27:59 -0500

    Fixed "fox-repl" test

--- test/fox-repl-spec.js
+++ test/fox-repl-spec.js
@@ -1,29 +1,46 @@
 var chai = require("chai");
 var expect = chai.expect;
 
+var vm = require("vm");
 var stream = require("stream");
 var StringDecoder = require("string_decoder").StringDecoder;
 
-var foxRepl = require("../lib/fox-repl")(null, { msg: "Hello" });
+var dbData = require("./data/test-db.json");
+var db = require("../lib/db").buildFromData(dbData);
+
+var foxShell = require("../lib/fox-shell")(db);
+var foxRepl = require("../lib/fox-repl")(foxShell, { msg: "Hello" });
 
 describe("fox-repl", function()
 {
+	var evalResult;
+	var callback = function(err, result)
+	{
+		if(err != null)
+		{
+			throw err;
+		}
+
+		evalResult = result;
+	};
+
+	before(function()
+	{
+		evalResult = null;
+	});
+
 	describe("eval(cmd, context, filename, callback)", function()
 	{
 		it("hands commands to the fox shell and default repl", function()
 		{
-			var evalResult = null;
-			foxRepl.eval("(7 + 13)", global, null, function(err, result)
-			{
-				if(err != null)
-				{
-					throw err;
-				}
+			var context = vm.createContext();
 
-				console.log(result);
-				evalResult = result;
-			});
+			foxRepl.eval("(7 + 13)", context, null, callback);
 			expect(evalResult).to.equal(20);
+
+			foxRepl.eval("#name", context, null, callback);
+			expect(evalResult.length).to.equal(1);
+			expect(evalResult[0].id).to.equal(db.tableIds.name);
 		});
 	});
 
